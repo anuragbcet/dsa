1151e0e09633618e087f0493b133e56b4919b9e2
anuragbcet <anuragbcet2023@gmail.com>
2025-02-23 19:19:25 +0530

    taking inputs

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
-class Main{
+import java.util.Scanner;
+
+public class Main{
     public static void main(String args[]){
         System.out.println("hello world!");
+        System.out.println("This is the first java programme made.");
+        Scanner sc= new Scanner(System.in);
+        int roll = sc.nextInt();
+        System.out.println("Your roll no is :"+roll);
     }
 }
