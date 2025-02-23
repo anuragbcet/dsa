8d01a8e681e54b01b03391042a386eeac543b149
anuragbcet <anuragbcet2023@gmail.com>
2025-02-23 19:18:15 +0530

    primite data types

---
+++ First.java
@@ -0,0 +1,14 @@
+public class First {
+    public static void main(String[] args) {
+        int a=25;
+        char b = 'c';
+        float c=96523.454f;
+        String d ="Anurag kumar";
+        double e = 45654215.5655;
+        System.out.println("Integer: "+a);
+        System.out.println("Character: "+b);
+        System.out.println("Float: "+c);
+        System.out.println("String: "+d);
+        System.out.println("double: "+e);
+    }
+}
